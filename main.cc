e08a87d9b0bc3c194b12747e88ae05c0b6e2f133
antony-jr <antonyjr@protonmail.com>
2019-05-26 23:25:38 +0530

    do not quit on last window

--- main.cc
+++ main.cc
@@ -5,6 +5,8 @@ int main(int ac , char **av){
 	QApplication::setApplicationName(QString::fromUtf8("Blue Glow"));
 	QApplication::setOrganizationName(QString::fromUtf8("The Future Shell Laboratory"));
 	QApplication app(ac,av);
+	app.setQuitOnLastWindowClosed(false);
+
 	BlueGlow guiMain;
 	guiMain.init();
 	QObject::connect(&guiMain , &BlueGlow::quit , &app , 
